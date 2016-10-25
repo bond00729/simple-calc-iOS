4a695072fcd7a8dbbabf8985215ccd35b486adfd
Matthew Bond <matthew.randal.bond@gmail.com>
2016-10-25 13:56:49 -0700

    everything working except factorial button

--- SimpleCalciOS/SimpleCalciOS/CalcViewController.swift
+++ SimpleCalciOS/SimpleCalciOS/CalcViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class CalcViewController: UIViewController {
 
-    private var seperate: Bool = false
-    private var exponentCalc: [Int] = []
     private var numbers: [Int] = []
-    private var operand: String = ""
+    private var allNumbers: [Int] = []
     private var left: Int = 0
     private var right: Int = 0
+    private var operand: String = ""
     @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
@@ -27,6 +26,19 @@ class CalcViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func changeLabel() {
+        if numbers.count > 0 {
+            var newLabelText = ""
+            for index in 0...numbers.count - 1 {
+                let num = numbers[index]
+                newLabelText += "\(num)"
+            }
+            self.displayLabel.text = newLabelText
+        } else {
+            self.displayLabel.text = ""
+        }
     }
     
     private func changeLabel(_ num: Int) {
@@ -46,93 +58,100 @@ class CalcViewController: UIViewController {
         }
     }
     
-    private func addToSide(_ num: Int) {
-        if seperate {
-            right += num * pow(10, exponentCalc.count - 1)
-            self.changeLabel(right)
-        } else {
-            left += num * pow(10, exponentCalc.count - 1)
-            self.changeLabel(left)
-        }
-    }
-    
     @IBAction func numberOne(_ sender: AnyObject) {
-        exponentCalc.append(1)
-        self.addToSide(1)
+        numbers.insert(1, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func numberTwo(_ sender: AnyObject) {
-        exponentCalc.append(2)
-        self.addToSide(2)
+        numbers.insert(2, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonThree(_ sender: AnyObject) {
-        exponentCalc.append(3)
-        self.addToSide(3)
+        numbers.insert(3, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonFour(_ sender: AnyObject) {
-        exponentCalc.append(4)
-        self.addToSide(4)
+        numbers.insert(4, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonFive(_ sender: AnyObject) {
-        exponentCalc.append(5)
-        self.addToSide(5)
+        numbers.insert(5, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonSix(_ sender: AnyObject) {
-        exponentCalc.append(6)
-        self.addToSide(6)
+        numbers.insert(6, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonSeven(_ sender: AnyObject) {
-        exponentCalc.append(7)
-        self.addToSide(7)
+        numbers.insert(7, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonEight(_ sender: AnyObject) {
-        exponentCalc.append(8)
-        self.addToSide(8)
+        numbers.insert(8, at: numbers.count)
+        self.changeLabel()
     }
     
     @IBAction func buttonNine(_ sender: AnyObject) {
-        exponentCalc.append(9)
-        self.addToSide(9)
+        numbers.insert(9, at: numbers.count)
+        self.changeLabel()
+    }
+    
+    @IBAction func buttonZero(_ sender: AnyObject) {
+        numbers.insert(0, at: numbers.count)
+        self.changeLabel()
+    }
+    
+    private func sum() -> Int {
+        var total = 0
+        var index = 0
+        while index < numbers.count {
+            total += numbers[index] * self.pow(10, numbers.count - (1 + index))
+            index += 1
+        }
+        return total;
     }
 
     @IBAction func add(_ sender: AnyObject) {
-        self.seperate = true
         self.operand = "+"
-        exponentCalc.removeAll()
+        self.left = self.sum()
+        numbers.removeAll()
     }
     
     @IBAction func subtract(_ sender: AnyObject) {
-        self.seperate = true
         self.operand = "-"
-        exponentCalc.removeAll()
+        self.left = self.sum()
+        numbers.removeAll()
     }
     
     @IBAction func multiply(_ sender: AnyObject) {
-        self.seperate = true
         self.operand = "*"
-        exponentCalc.removeAll()
+        self.left = self.sum()
+        numbers.removeAll()
     }
     
     @IBAction func divide(_ sender: AnyObject) {
-        self.seperate = true
         self.operand = "/"
-        exponentCalc.removeAll()
+        self.left = self.sum()
+        numbers.removeAll()
     }
     
     @IBAction func mod(_ sender: AnyObject) {
-        self.seperate = true
         self.operand = "%"
-        exponentCalc.removeAll()
+        self.left = self.sum()
+        numbers.removeAll()
     }
     
     @IBAction func equal(_ sender: AnyObject) {
+        self.right = self.sum()
         if self.operand == "+" {
+            print("\(left) + \(right)")
             self.changeLabel(left + right)
         } else if self.operand == "-" {
             self.changeLabel(left - right)
@@ -143,33 +162,33 @@ class CalcViewController: UIViewController {
         } else if self.operand == "%" {
             self.changeLabel(left % right)
         }
-        exponentCalc.removeAll()
-        numbers.append(left)
-        numbers.append(right)
+        numbers.removeAll()
+        allNumbers.append(left)
+        allNumbers.append(right)
     }
-    
+
     @IBAction func count(_ sender: AnyObject) {
-        self.changeLabel(numbers.count)
+        self.changeLabel(allNumbers.count)
     }
     
     @IBAction func average(_ sender: AnyObject) {
         var total = 0
-        for index in 0...numbers.count - 1 {
-            total += numbers[index]
+        for index in 0...allNumbers.count - 1 {
+            total += allNumbers[index]
         }
-        self.changeLabel(total / numbers.count)
+        self.changeLabel(total / allNumbers.count)
     }
     
     @IBAction func factorial(_ sender: AnyObject) {
-        var total = 0
-        for index in 0...numbers.count - 1 {
-            total += numbers[index]
-        }
-        self.changeLabel(total / numbers.count)
+        
     }
     
     @IBAction func clear(_ sender: AnyObject) {
-        exponentCalc.removeAll()
         numbers.removeAll()
+        allNumbers.removeAll()
+        self.changeLabel()
+        self.left = 0
+        self.right = 0
+        self.operand = ""
     }
 }
